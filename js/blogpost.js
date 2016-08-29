a0ba63aa3d9e65ccdfb06effe82617ef5b4dbb6c
Comfort AJala <Comfort AJala>
2016-08-29 20:19:11 +0200

    Added social icons and the keywords section

--- js/blogpost.js
+++ js/blogpost.js
@@ -10,7 +10,8 @@ var blogPosts = [
 	content:"Hello, my name is Comfort Ajala and this is my first blog post! Look forward to the next! ",
 	date:"27.08.2016",
 	author:"Me",
-	images:[]
+	images:[],
+	keyword:["general"]
 	
 },
 {
@@ -21,7 +22,8 @@ var blogPosts = [
 	content:"",
 	date:"29.08.2016",
 	author:"Me",
-	images:[{title:"Original Writing",link:"first.jpg",text:"I made a lot of mistakes in my original writing, but I understand grossly what I did wrong."},{title:"First Correction",link:"first 1.jpg",text:""},{title:"Second Correction",link:"first 2.jpg",text:"The most mistakes made were not grammatical mistakes but rather \"contextual\" errors. So you know there are words or phrases that fit better in certain situations where others do not. Yep, that was my biggest error here. This is why we have to keep writing more and practicing in order to get acquainted with the feel of the language and know when to use vocabulary learned. Do not just memorise words, learn when to use them."}]
+	images:[{title:"Original Writing",link:"first.jpg",text:"I made a lot of mistakes in my original writing, but I understand grossly what I did wrong."},{title:"First Correction",link:"first 1.jpg",text:""},{title:"Second Correction",link:"first 2.jpg",text:"The most mistakes made were not grammatical mistakes but rather \"contextual\" errors. So you know there are words or phrases that fit better in certain situations where others do not. Yep, that was my biggest error here. This is why we have to keep writing more and practicing in order to get acquainted with the feel of the language and know when to use vocabulary learned. Do not just memorise words, learn when to use them."}],
+	keyword:["korean"]
 	
 }
 	
