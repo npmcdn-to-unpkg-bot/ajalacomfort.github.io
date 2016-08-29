fec5bc04013cacf349b817b93f818f7b9292e5a8
Comfort AJala <Comfort AJala>
2016-08-29 05:26:13 +0200

    Updated the fill system and added the Why I like Korean drama blog post

---
+++ js/blogpost.js
@@ -0,0 +1,33 @@
+
+
+var blogPosts = [
+	
+{
+	
+	
+	title: "Hello World, I Blogeth!!!!",
+	videos:[],
+	content:"Hello, my name is Comfort Ajala and this is my first blog post! Look forward to the next! ",
+	date:"27.08.2016",
+	author:"Me",
+	images:[]
+	
+},
+{
+	
+	
+	title:"Why I like Korean Dramas (in Korean)",
+	videos:[{title:"Day 6 of speaking in Korean Youtube Report",link:"https://www.youtube.com/embed/-WrxjBZrVtg",text:"I really feel like I am making huge progress by uploading these videos! look forward to the future ones!"}],
+	content:"",
+	date:"29.08.2016",
+	author:"Me",
+	images:[{title:"Original Writing",link:"first.jpg",text:"I made a lot of mistakes in my original writing, but I understand grossly what I did wrong."},{title:"First Correction",link:"first 1.jpg",text:""},{title:"Second Correction",link:"first 2.jpg",text:"The most mistakes made were not grammatical mistakes but rather \"contextual\" errors. So you know there are words or phrases that fit better in certain situations where others do not. Yep, that was my biggest error here. This is why we have to keep writing more and practicing in order to get acquainted with the feel of the language and know when to use vocabulary learned. Do not just memorise words, learn when to use them."}]
+	
+}
+	
+
+	
+	
+	
+	
+	]
--- js/reactrocks.js
+++ js/reactrocks.js
@@ -19,6 +19,25 @@
 							
 												
 														{this.state.posts.map(function(post){
+															
+															var images = post.images.map(function(image){
+																
+																	return(
+																	<div className="post_image_group"> 
+																	<h4>{image.title}</h4>
+																	<img src={"images/" + image.link} className="post_image"/>
+																	<p>{image.text}</p>
+																	</div>)
+															})
+															
+															var embed_videos = post.videos.map(function(video){
+																	return (
+																		<div className="embeddedVideos">
+																				<h4>{video.title}</h4>
+																					 <iframe width="100%" height="500" src={video.link} frameborder="0" allowfullscreen></iframe> 
+																				<p>{video.text}</p>
+																		</div>)
+															})
 																
 																	return (
 																
@@ -27,7 +46,11 @@
 																			<p className="title">{post.title}</p>
 																			<p className="post_author">By: {post.author}</p>
 																			<p className="post_date">Written on : {post.date}</p>
+																			{images}
+														
 																			<p className="post_content">{post.content}</p>
+																			
+																			{embed_videos}		
 																
 																		</div>
 																	
